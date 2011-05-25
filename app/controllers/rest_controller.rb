e83f34aaee1721a0c7f794b2e4d4dd95ad8f18a1
Schimodie <radu.gagos@gmail.com>
2011-05-25 20:19:31 +0300

    modified the rest constroller

--- app/controllers/rest_controller.rb
+++ app/controllers/rest_controller.rb
@@ -1,5 +1,16 @@
+require 'csv'
+
 class RestController < ApplicationController
-  def api
+  def initialize 
+    csv = CSV::parse(File.open(Rails.root + 'contest.csv') { |f| f.read })
+    csvFields = csv.shift
+    @objects = []
+  
+    csv.each do |record|
+      @objects += [record]
+    end
   end
 
+  def api
+  end
 end
