d89f0e77f8391dcaf5d4c2fcc307974389ec65ee
Schimodie <radu.gagos@gmail.com>
2011-05-27 16:39:09 +0300

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Contest31::Application.routes.draw do
   root :to => 'rest#home'
-  
+
   match '/rest/projects/show/:id(.:format)' => 'rest#show_id'
   match '/rest/projects/updated-list(.:format)' => 'rest#show_count'
 
-  # match ':controller(/:action(/:id(.:format)))'
+  match '/rest/api' => 'rest#api'
 end
